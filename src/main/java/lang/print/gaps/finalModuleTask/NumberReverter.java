0671785c1f32a6ec17476d6fe4fc1e96986cb18a
Giorgi Tsintsadze <giorgi.tsintsadze@scnadiweb.com>
2023-08-09 22:30:19 +0400

    Final task

--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,13 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int reversedNumber = 0;
+        int digit;
+
+        while (number > 0) {
+            digit = number % 10;
+            reversedNumber = reversedNumber * 10 + digit;
+            number /= 10;
+        }
     }
 }
